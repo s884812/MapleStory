5fe4c5d798c75673aae8c82ecbcaccaacda3bcbd
Rahul <dumbersoft@gmail.com>
2016-12-26 20:08:35 -0500

    MapleStory v83

--- Quick Quest Creator.bat
+++ Quick Quest Creator.bat
@@ -1,5 +1,5 @@
 @echo off 
 @title AuroStory Quick Quest Creator 
-set CLASSPATH=.;lib\* 
-java -Xmx10m -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd tools.QuestCreator 
+set CLASSPATH=.;dist\* 
+java -Xmx10m -Djavax.net.ssl.keyStore=filename.keystore -Djavax.net.ssl.keyStorePassword=passwd -Djavax.net.ssl.trustStore=filename.keystore -Djavax.net.ssl.trustStorePassword=passwd tools.QuestCreator
 pause  
